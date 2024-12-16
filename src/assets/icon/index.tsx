3cf99e054a766fd2fa5c05de3288daf2c4f9f099
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-16 14:25:46 +0100

    import some icons

---
+++ src/assets/icon/index.tsx
@@ -0,0 +1,5 @@
+import facebookIcon from "./facebook-lg.svg";
+import instagramIcon from "./instagram-lg.svg";
+import youtubeIcon from "./youtube-lg.svg";
+
+export { facebookIcon, instagramIcon, youtubeIcon };
