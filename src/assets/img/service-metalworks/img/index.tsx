be40116e466a19250c7e5c83d707a84284e0ffbb
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-16 14:43:14 +0100

    import metalworks images

---
+++ src/assets/img/service-metalworks/img/index.tsx
@@ -0,0 +1,7 @@
+import metalWorksImg1 from "./137f790a-e6c6-4b16-adc6-aa60efd37b48.jpg";
+import metalWorksImg2 from "./13a9158e-0750-4f0d-9400-bca0c5129766.jpg";
+import metalWorksImg3 from "./415fbe6b-9489-44b9-b909-692421df0ebe.jpg";
+import metalWorksImg4 from "./8e21728d-3b23-4b6c-a39a-095282fa6a58.jpg";
+import metalWorksImg5 from "./da338e8c-a6a3-4b5a-b220-7b783d58b016.jpg";
+
+export { metalWorksImg1, metalWorksImg2, metalWorksImg3, metalWorksImg4, metalWorksImg5 };
--- src/assets/img/service-metalworks/index.tsx
+++ src/assets/img/service-metalworks/index.tsx
@@ -1,5 +1,6 @@
 import metalWorksServiceCardImg from "./card-image.jpg";
 import metalWorksServiceBannerLG from "./main-header-lg.jpg";
 import metalWorksServiceBannerSM from "./main-header-sm.jpg";
+import { metalWorksImg1, metalWorksImg2, metalWorksImg3, metalWorksImg4, metalWorksImg5 } from "./img";
 
-export { metalWorksServiceCardImg, metalWorksServiceBannerLG, metalWorksServiceBannerSM };
+export { metalWorksServiceCardImg, metalWorksServiceBannerLG, metalWorksServiceBannerSM, metalWorksImg1, metalWorksImg2, metalWorksImg3, metalWorksImg4, metalWorksImg5 };
