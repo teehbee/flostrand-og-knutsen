20ceb52671b01cec2bdbcb47d8576f6e19d4b1e4
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-16 14:31:33 +0100

    import banners

---
+++ src/assets/img/index.tsx
@@ -0,0 +1,8 @@
+import mainHeaderLarge from "./main-header-lg.jpg";
+import mainHeaderSmall from "./main-header-sm.jpg";
+import aboutMainHeaderLarge from "./about-upper-1920.jpg";
+import aboutMainHeaderSmall from "./about-upper-640.jpg";
+import aboutSecondaryHeaderLarge from "./about-header-1920.jpg";
+import aboutSecondaryHeaderSmall from "./about-header-640.jpg";
+
+export { mainHeaderLarge, mainHeaderSmall, aboutMainHeaderLarge, aboutMainHeaderSmall, aboutSecondaryHeaderLarge, aboutSecondaryHeaderSmall };
