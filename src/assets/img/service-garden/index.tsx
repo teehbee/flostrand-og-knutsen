7bd0805d69a817ef343b793f982b7c307e872298
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-16 14:40:56 +0100

    improt banners and card img for all services

---
+++ src/assets/img/service-garden/index.tsx
@@ -0,0 +1,5 @@
+import gardenServiceCardImg from "./card-image.jpg";
+import gardenServiceBannerLG from "./header-lg.jpg";
+import gardenServiceBannerSM from "./header-sm.jpg";
+
+export { gardenServiceCardImg, gardenServiceBannerLG, gardenServiceBannerSM };
---
+++ src/assets/img/service-metalworks/index.tsx
@@ -0,0 +1,5 @@
+import metalWorksServiceCardImg from "./card-image.jpg";
+import metalWorksServiceBannerLG from "./main-header-lg.jpg";
+import metalWorksServiceBannerSM from "./main-header-sm.jpg";
+
+export { metalWorksServiceCardImg, metalWorksServiceBannerLG, metalWorksServiceBannerSM };
