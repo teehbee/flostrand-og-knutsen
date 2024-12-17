6c14819fc9a510a56ee106e79705a39f217db2bb
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-17 09:29:53 +0100

    header styling

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -22,49 +22,57 @@ function Header() {
           </Link>
           <img src={menuBars} className="d-lg-none nav-bars-button" aria-label="menu-button" onClick={handleShow} />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto main-navigation">
-              <NavDropdown title="Tjenester" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#tjeneste1">Tjeneste 1</NavDropdown.Item>
-                <NavDropdown.Item href="#tjeneste1">Tjeneste 2</NavDropdown.Item>
-                <NavDropdown.Item href="#tjeneste1">Tjeneste 3</NavDropdown.Item>
-                <NavDropdown.Item href="#tjeneste1">Tjeneste 4</NavDropdown.Item>
+            <Nav className="ms-auto main-navigation dark-font">
+              <NavDropdown title="Tjenester" id="basic-nav-dropdown" className="dark-font fs-1-25rem fw-500">
+                <NavDropdown.Item href="#tjeneste1" className="dark-font fs-1-25rem fw-500">
+                  Tjeneste 1
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#tjeneste1" className="dark-font fs-1-25rem fw-500">
+                  Tjeneste 2
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#tjeneste1" className="dark-font fs-1-25rem fw-500">
+                  Tjeneste 3
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#tjeneste1" className="dark-font fs-1-25rem fw-500">
+                  Tjeneste 4
+                </NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link href="/about" className="btn">
+              <Nav.Link href="/about" className="btn dark-font fs-1-25rem fw-500">
                 Om oss
               </Nav.Link>
-              <Nav.Link href="/contact" className="btn">
+              <Nav.Link href="/contact" className="btn dark-font fs-1-25rem fw-500">
                 Kontakt
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </div>
       </Navbar>
-      <Offcanvas className="gray-color d-block d-lg-none h100" show={show} onHide={handleClose} responsive="lg">
+      <Offcanvas className="gray-color d-block d-lg-none pe-3 " show={show} onHide={handleClose} responsive="lg">
         <Offcanvas.Header closeButton>
           <Offcanvas.Title className="">
             <img src={mainLogo} className="header-logo" aria-label="logo" />
           </Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body className="d-flex flex-column align-items-start h100">
-          <Navbar.Text className="py-1 fw-bold">Tjenester</Navbar.Text>
+        <Offcanvas.Body className="d-flex flex-column align-items-start w-100">
+          <Navbar.Text className="py-1 fw-bold dark-font dropdown-border">Tjenester</Navbar.Text>
           <div>
-            <Nav.Link href="/tjeneste1" className="btn py-1">
+            <Nav.Link href="/tjeneste1" className="btn py-1 dark-font text-indent-dropdown fw-500">
               Tjeneste 1
             </Nav.Link>
-            <Nav.Link href="/tjeneste2" className="btn py-1">
+            <Nav.Link href="/tjeneste2" className="btn py-1 dark-font text-indent-dropdown fw-500">
               Tjeneste 2
             </Nav.Link>
-            <Nav.Link href="/tjeneste3" className="btn py-1">
+            <Nav.Link href="/tjeneste3" className="btn py-1 dark-font text-indent-dropdown fw-500">
               Tjeneste 3
             </Nav.Link>
-            <Nav.Link href="/tjeneste4" className="btn py-1">
+            <Nav.Link href="/tjeneste4" className="btn py-1 dark-font text-indent-dropdown fw-500">
               Tjeneste 4
             </Nav.Link>
           </div>
-          <Nav.Link href="/about" className="btn py-1">
+          <Nav.Link href="/about" className="btn py-1 dark-font fw-500">
             Om oss
           </Nav.Link>
-          <Nav.Link href="/contact" className="btn py-1">
+          <Nav.Link href="/contact" className="btn py-1 dark-font fw-500">
             Kontakt
           </Nav.Link>
         </Offcanvas.Body>
