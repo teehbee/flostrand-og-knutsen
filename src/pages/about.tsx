f3a99a77763db12d48a9afd519d7a8155a39a9f9
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-26 15:48:05 +0100

    add banner image on about page and banner content placed on image

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,17 @@
+import { aboutMainHeaderLarge, aboutMainHeaderSmall, aboutSecondaryHeaderLarge, aboutSecondaryHeaderSmall } from "../assets/img";
+
 function About() {
-  return <div>About</div>;
+  return (
+    <section id="about-us-landing" className="position-relative">
+      <picture>
+        <source media="(min-width: 992px)" srcSet={aboutMainHeaderLarge} />
+        <img className="frontpage-banner" src={aboutMainHeaderSmall} alt="om oss" />
+      </picture>
+      <div className="banner-content container">
+        <h1 className="text-light">Hvem er vi?</h1>
+      </div>
+    </section>
+  );
 }
 
 export default About;
