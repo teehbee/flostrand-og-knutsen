15739c9ddda894602a56c767986780bb98ac6db6
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-01-02 14:21:30 +0100

    change structure and dynamically add lower text on about page

--- src/components/about/content/index.tsx
+++ src/components/about/content/index.tsx
@@ -1,7 +1,3 @@
-import { aboutLandingImageDescription, aboutHeader, aboutLowerImageDescription, aboutLowerImageHeader, aboutLowerImageText, aboutMainParagraph, aboutMainParagraphSignature } from "./textContent";
+import { aboutLandingImageDescription, aboutHeader, aboutLowerImageDescription, aboutLowerImageHeader, aboutLowerImageText, aboutMainParagraph, aboutMainParagraphSignature, aboutSecondaryParagraphs, aboutServicesTitle, aboutServicesTitlesAndContents } from "./textContent";
 
-import { aboutLowerParagraph1Bold, aboutLowerParagraph1, aboutLowerParagraph2Bold, aboutLowerParagraph2, aboutLowerParagraph3Bold, aboutLowerParagraph3, aboutLowerParagraph4Bold, aboutLowerParagraph4, aboutLowerParagraph5Bold, aboutLowerParagraph5 } from "./textContent";
-
-import { aboutServicesTitle, aboutServicesTitlesAndContents } from "./textContent";
-
-export { aboutLandingImageDescription, aboutHeader, aboutLowerImageDescription, aboutLowerImageHeader, aboutLowerImageText, aboutMainParagraph, aboutMainParagraphSignature, aboutLowerParagraph1Bold, aboutLowerParagraph1, aboutLowerParagraph2Bold, aboutLowerParagraph2, aboutLowerParagraph3Bold, aboutLowerParagraph3, aboutLowerParagraph4Bold, aboutLowerParagraph4, aboutLowerParagraph5Bold, aboutLowerParagraph5, aboutServicesTitle, aboutServicesTitlesAndContents };
+export { aboutLandingImageDescription, aboutHeader, aboutLowerImageDescription, aboutLowerImageHeader, aboutLowerImageText, aboutMainParagraph, aboutMainParagraphSignature, aboutSecondaryParagraphs, aboutServicesTitle, aboutServicesTitlesAndContents };
--- src/components/about/sections/AboutLowerTextBody.tsx
+++ src/components/about/sections/AboutLowerTextBody.tsx
@@ -1,14 +1,12 @@
-import { aboutLowerParagraph1Bold, aboutLowerParagraph1, aboutLowerParagraph2Bold, aboutLowerParagraph2, aboutLowerParagraph3Bold, aboutLowerParagraph3, aboutLowerParagraph4Bold, aboutLowerParagraph4, aboutLowerParagraph5Bold, aboutLowerParagraph5 } from "../content";
+import { aboutSecondaryParagraphs } from "../content";
 import { AboutTextParagraph } from "../elements";
 
 function AboutLowerTextBody() {
   return (
     <section id="about-us-lower-text" className="container py-5 my-5">
-      <AboutTextParagraph textContentBold={aboutLowerParagraph1Bold} textContent={aboutLowerParagraph1} />
-      <AboutTextParagraph textContentBold={aboutLowerParagraph2Bold} textContent={aboutLowerParagraph2} />
-      <AboutTextParagraph textContentBold={aboutLowerParagraph3Bold} textContent={aboutLowerParagraph3} />
-      <AboutTextParagraph textContentBold={aboutLowerParagraph4Bold} textContent={aboutLowerParagraph4} />
-      <AboutTextParagraph textContentBold={aboutLowerParagraph5Bold} textContent={aboutLowerParagraph5} />
+      {aboutSecondaryParagraphs.map((paragraph, index) => (
+        <AboutTextParagraph key={index} bold={paragraph.bold} main={paragraph.main} />
+      ))}
     </section>
   );
 }
