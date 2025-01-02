0ce1aa22ecfad540e43e4661408b1e6045f8bb57
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-01-02 15:18:52 +0100

    add links to tiles on frontpage

--- src/components/frontpage/elements/ServicesTile.tsx
+++ src/components/frontpage/elements/ServicesTile.tsx
@@ -3,17 +3,18 @@ import { Link } from "react-router-dom";
 interface serviceTile {
   serviceImage: string;
   serviceTitle: string;
+  link: string;
 }
 
-const ServicesTile: React.FC<serviceTile> = ({ serviceImage, serviceTitle }) => {
+const ServicesTile: React.FC<serviceTile> = ({ serviceImage, serviceTitle, link }) => {
   return (
     <div className="services-card col-12 col-md-6 col-lg-3 rounded p-0 my-1">
       <div className="position-relative">
-        <Link to="/service">
+        <Link to={link}>
           <img className="img-fluid rounded" src={serviceImage} alt="tjeneste link" />
         </Link>
         <div className="position-absolute bottom-0 start-0">
-          <Link to="/tjeneste1">
+          <Link to={link}>
             <button className="m-3 transparent-button">{serviceTitle}</button>
           </Link>
         </div>
--- src/components/frontpage/sections/serviceTiles.tsx
+++ src/components/frontpage/sections/serviceTiles.tsx
@@ -4,10 +4,10 @@ import { marineServiceCardImg, personnelServiceCardImg, metalWorksServiceCardImg
 function ServicesTiles() {
   return (
     <div className="d-flex my-5 py-lg-5 flex-wrap justify-content-center justify-content-md-between">
-      <ServicesTile serviceImage={marineServiceCardImg} serviceTitle="Marine deck fabrication" />
-      <ServicesTile serviceImage={metalWorksServiceCardImg} serviceTitle="Metallarbeid" />
-      <ServicesTile serviceImage={personnelServiceCardImg} serviceTitle="Personell" />
-      <ServicesTile serviceImage={gardenServiceCardImg} serviceTitle="Hagearbeid" />
+      <ServicesTile serviceImage={marineServiceCardImg} serviceTitle="Marine deck fabrication" link="/marine-deck-fabrication" />
+      <ServicesTile serviceImage={metalWorksServiceCardImg} serviceTitle="Metallarbeid" link="/metal-works" />
+      <ServicesTile serviceImage={personnelServiceCardImg} serviceTitle="Personell" link="/personel" />
+      <ServicesTile serviceImage={gardenServiceCardImg} serviceTitle="Hagearbeid" link="/garden-works" />
     </div>
   );
 }
