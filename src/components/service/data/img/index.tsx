cf97eabddc7d1f12e0c6d188a5f6b2dd25b17030
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-01-02 16:05:13 +0100

    import images for metalworks gallery

---
+++ src/components/service/data/img/index.tsx
@@ -0,0 +1,7 @@
+import metalImg1 from "../../../../assets/img/service-metalworks/img/137f790a-e6c6-4b16-adc6-aa60efd37b48.jpg";
+import metalImg2 from "../../../../assets/img/service-metalworks/img/13a9158e-0750-4f0d-9400-bca0c5129766.jpg";
+import metalImg3 from "../../../../assets/img/service-metalworks/img/415fbe6b-9489-44b9-b909-692421df0ebe.jpg";
+import metalImg4 from "../../../../assets/img/service-metalworks/img/8e21728d-3b23-4b6c-a39a-095282fa6a58.jpg";
+import metalImg5 from "../../../../assets/img/service-metalworks/img/da338e8c-a6a3-4b5a-b220-7b783d58b016.jpg";
+
+export { metalImg1, metalImg2, metalImg3, metalImg4, metalImg5 };
