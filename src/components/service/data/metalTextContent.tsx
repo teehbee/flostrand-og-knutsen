572f9f94f509e11be25203156b2d7a4f549b1c68
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-01-02 15:50:06 +0100

    add text to personel page

--- src/components/service/data/metalTextContent.tsx
+++ src/components/service/data/metalTextContent.tsx
@@ -2,6 +2,7 @@ export const metalworksHeader = "Header goes here";
 
 export const metalworksText = [
   {
-    text: "Sveiser alle metaller – fra masseforflytning til tynnvegget aluminium og rustfritt stål. Med nødvendig kompetanse og titalls år med erfaring, leverer jeg løsninger med høy presisjon og kvalitet. Holder kunder og samarbeidspartnere tett involvert under produksjonen for kontinuerlig samarbeid og tilpasning gjennom hele prosessen.",
+    title: "Sveiser alle metaller – fra masseforflytning til tynnvegget aluminium og rustfritt stål.",
+    text: "Med nødvendig kompetanse og titalls år med erfaring, leverer jeg løsninger med høy presisjon og kvalitet. Holder kunder og samarbeidspartnere tett involvert under produksjonen for kontinuerlig samarbeid og tilpasning gjennom hele prosessen.",
   },
 ];
--- src/components/service/data/personelTextContent.tsx
+++ src/components/service/data/personelTextContent.tsx
@@ -1,7 +1,36 @@
-export const personelHeader = "Header goes here";
+export const personelHeader = "Utleie av Spesialiserte Tjenester";
 
 export const personelText = [
   {
-    text: "Personel text",
+    title: "Sveising og Platearbeid",
+    text: "Vi tilbyr kvalifisert personell innen sveising og platearbeid. Våre erfarne fagfolk har den nødvendige kompetansen for å håndtere prosjekter av ulik størrelse, og vi sikrer høy kvalitet og presisjon i alt arbeid som utføres.",
+  },
+  {
+    title: "Prosjektledere i Produksjon",
+    text: "Våre dyktige prosjektledere har solid erfaring fra produksjonsmiljøer og verksteder. De er rustet til å lede prosjekter effektivt, koordinere team og sikre at tidsfrister og kvalitetskrav oppfylles.",
+  },
+  {
+    title: "Sjøfolk og Matroser",
+    text: "Vi tilbyr også erfarne sjøfolk og matroser som kan støtte driften ombord. Med omfattende kunnskap om sjøfart og navigasjon bidrar de til en trygg og effektiv seilas.",
+  },
+  {
+    title: "Motorpassere med Teknisk Skipsforståelse",
+    text: "Våre motorpassere besitter dyp teknisk forståelse for skipsmaskiner og -systemer. De håndterer vedlikehold og reparasjoner, og sikrer at alle tekniske aspekter ombord er i topp stand.",
+  },
+  {
+    title: "Sertifiserte Maskinførere",
+    text: "Vi har sertifiserte maskinførere klare til å betjene et bredt spekter av maskiner, inkludert:",
+  },
+  {
+    title: "",
+    text: "Gravemaskin, hjulaster, dumper, C2 Teleskoptruck, truckfører",
+  },
+  {
+    title: "",
+    text: "Våre maskinførere har de nødvendige sertifiseringene og erfaringen for å utføre oppgaver trygt og effektivt, uansett prosjektets omfang.",
+  },
+  {
+    title: "Ta kontakt med oss i dag for å finne ut hvordan vi kan bidra til din suksess!",
+    text: "",
   },
 ];
