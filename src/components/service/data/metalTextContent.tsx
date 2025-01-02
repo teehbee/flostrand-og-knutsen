10be1c85d478553e6dc58cce31f2815d4566384e
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-01-02 16:13:58 +0100

    add array of images for metalworks

--- src/components/service/data/metalTextContent.tsx
+++ src/components/service/data/metalTextContent.tsx
@@ -1,3 +1,5 @@
+import { metalWorksImg1, metalWorksImg2, metalWorksImg3, metalWorksImg4, metalWorksImg5 } from "../../../assets/img/service-metalworks";
+
 export const metalworksHeader = "Trenger en header";
 
 export const metalworksText = [
@@ -6,3 +8,5 @@ export const metalworksText = [
     text: "Med nødvendig kompetanse og titalls år med erfaring, leverer jeg løsninger med høy presisjon og kvalitet. Holder kunder og samarbeidspartnere tett involvert under produksjonen for kontinuerlig samarbeid og tilpasning gjennom hele prosessen.",
   },
 ];
+
+export const metalWorksImages = [{ metalWorksImg1 }, { metalWorksImg2 }, { metalWorksImg3 }, { metalWorksImg4 }, { metalWorksImg5 }];
